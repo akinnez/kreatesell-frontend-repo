64b75bfff80962ffb9f4b01c2f9e5dcc1ce4341d
therealmrgabi <gabirated@gmail.com>
2021-07-29 09:43:24 +0100

    complete dashboard analytics

--- pages/account/dashboard/index.js
+++ pages/account/dashboard/index.js
@@ -1,9 +1,16 @@
 // import { format } from "date-fns";
+import { useState, useEffect } from "react";
+import { Modal, Button } from "../../../components";
 import { AuthLayout } from "../../../components/authlayout";
 import styles from "../../../public/css/Dashboard.module.scss";
 import { RecentAnalytics, StatsCard, DateHeader } from "./partials";
 
 const Dashboard = () => {
+	const [modalVisible, setVisible] = useState(false);
+
+	useEffect(() => {
+		setVisible(true);
+	}, []);
 	return (
 		<AuthLayout>
 			<div className={styles.container}>
@@ -26,6 +33,36 @@ const Dashboard = () => {
 				/>
 
 				<RecentAnalytics />
+
+				<Modal
+					onClose={() => setVisible(false)}
+					visible={modalVisible}
+					className={styles.modal}
+					cancelPropagation={true}
+				>
+					<div className={styles.dashboardModal}>
+						<div className={styles.modalTitle}>
+							You're just getting started.
+						</div>
+						<p className={styles.content}>
+							Welcome to Kreatesell, your simple ecommerce tool to sell your
+							content, products <br /> and services across borders.
+						</p>
+						<div className={styles.buttonContainer}>
+							<Button
+								text="Setup Store"
+								bgColor="white"
+								className={styles.tipBtn}
+							/>
+							<Button
+								text="Proceed to Dashboard"
+								bgColor="blue"
+								className={styles.proceedBtn}
+								onClick={() => setVisible(false)}
+							/>
+						</div>
+					</div>
+				</Modal>
 			</div>
 		</AuthLayout>
 	);
