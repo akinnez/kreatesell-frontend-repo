dbddee7687136bbeef2adf10203092c0b01951c1
therealmrgabi <gabirated@gmail.com>
2021-08-15 12:37:41 +0100

    mask and unmask password

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import { useStore } from "../redux/store";
 import "../public/css/global.scss";
 import "react-dates/initialize";
+import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
 	const store = useStore(pageProps.initialReduxState);
