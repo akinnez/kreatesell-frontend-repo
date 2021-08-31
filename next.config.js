ed7e255c9f2e14c884787b277889c21f79e5607a
therealmrgabi <gabirated@gmail.com>
2021-08-31 04:18:33 +0100

    base url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	env: {
-		// BASE_URL: "https://api.kreatesell.com/api/",
-		BASE_URL: "http://kreatesellcom-001-site1.ctempurl.com/api/",
+		BASE_URL: "https://api.kreatesell.com/api/",
 	},
 };
