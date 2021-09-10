d02556640e6116ba97cff65e08999d8a5630898d
Salvo Agency <salvoprograms@gmail.com>
2021-09-10 13:28:06 +0000

    next.config.js edited online with Bitbucket

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
 	env: {
 		// BASE_URL: "http://kreatesellcom-001-site1.ctempurl.com/api/",
 		BASE_URL: "https://api.kreatesell.com/api/",
-		NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6LcHGn0bAAAAALTtYkBrcDA1pbaRiSXmkADz8khK",
+		NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB",
 	},
 };
