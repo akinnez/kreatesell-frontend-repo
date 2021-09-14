4aa73d52516f8adba6d4aa22221e6d56e7e7260c
therealmrgabi <gabirated@gmail.com>
2021-09-14 12:14:47 +0100

    changed env variable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	env: {
-		// BASE_URL: "http://kreatesellcom-001-site1.ctempurl.com/api/",
-		BASE_URL: "https://api.kreatesell.com/api/",
+		BASE_URL: "https://kreatesell.io/api/",
 		NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB",
 	},
 };
