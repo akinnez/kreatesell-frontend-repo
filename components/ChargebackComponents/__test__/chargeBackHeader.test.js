2ad18043e645ec3f5a6f93cf49d27049e3d46f8a
maxinth <tunde.j.adeleye@gmail.com>
2022-01-31 09:33:48 +0100

    added snapshots for chargeback components

---
+++ components/ChargebackComponents/__test__/chargeBackHeader.test.js
@@ -0,0 +1,17 @@
+import { ChargeBackHeader } from "../ChargeBackHeader";
+import renderer from "react-test-renderer";
+
+const mockProps = {
+  handleSearchInput: jest.fn(),
+  handleDurationInput: jest.fn(),
+  handleProductStatus: jest.fn(),
+  handleStartDate: jest.fn(),
+  handleEndDate: jest.fn(),
+  handleDateToInput: jest.fn(),
+  handleSearchSubmit: jest.fn(),
+};
+
+it("renders a snapshot of <ChargeBackHeader /> with mock props", () => {
+  const el = renderer.create(<ChargeBackHeader {...mockProps} />).toJSON();
+  expect(el).toMatchSnapshot();
+});
