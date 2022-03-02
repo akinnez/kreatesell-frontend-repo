f7287dcd1676981a04161a2fc77978ad41e60b56
maxinth <tunde.j.adeleye@gmail.com>
2022-03-02 10:20:43 +0100

    added snaps for recoveryStatus, recoveryStatusFilters and Tags

---
+++ components/kreatorAbandonedCarts/components/RecoveryStatus/__test__/recoveryStatus.test.js
@@ -0,0 +1,21 @@
+import RecoveryStatus from "..";
+import renderer from "react-test-renderer";
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+it("renders a snapshot of <RecoveryStatus />", () => {
+  const status = renderer.create(<RecoveryStatus />).toJSON();
+  expect(status).toMatchSnapshot();
+});
