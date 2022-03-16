bcfe07407eaad7b99df407a686be7893da36c811
maxinth <tunde.j.adeleye@gmail.com>
2022-03-16 15:45:02 +0100

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {
   InputButton,
   Layout,
@@ -33,25 +33,38 @@ import {
 import Image from "next/image";
 
 export default function Home() {
+  const [mail, setEmail] = useState("");
+
   return (
     <Layout subFooter={false} defaultMarginTop={true}>
       <div className={styles.container}>
-        <div className={styles.inputContainer}>
-          <InputButton
-            name="email"
-            placeholder="Enter your email..."
-            buttonText="Get Started Free"
-            buttonIcon={<RightArrow />}
-            onChange={(e) => setEmail(e.target.value)}
-            onSubmit={(e) => {
-              e.preventDefault();
-              router.push({
-                pathname: "/signup",
-                query: { email },
-              });
-            }}
-          />
-        </div>
+        <section className={styles.mainBody}>
+          <div className={styles.left}>
+            <h1>
+              Are you ready to experience a mind-blowing platform made just for
+              you to sell all your content and digital products across borders,
+              and massively earn without any hassle?
+            </h1>
+            <h2>Get ready! We are launching soon.</h2>
+            <p className={styles.text}>
+              To be the first to know when we launch, join the wait-list by
+              submitting your details below.
+            </p>
+          </div>
+          <div className={styles.inputContainer}>
+            <InputButton
+              name="email "
+              placeholder="Enter your email..."
+              buttonText="Submit"
+              buttonIcon={<RightArrow />}
+              onChange={(e) => setEmail(e.target.value)}
+              onSubmit={(e) => {
+                e.preventDefault();
+              }}
+            />
+            <p className={styles.textC}>KreateSell loading...</p>
+          </div>
+        </section>
       </div>
     </Layout>
   );
