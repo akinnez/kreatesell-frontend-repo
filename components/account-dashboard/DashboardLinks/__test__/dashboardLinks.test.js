58b1b6816ab91b7960607c355bef188d58d8c9e5
maxinth <tunde.j.adeleye@gmail.com>
2022-03-25 10:35:16 +0100

    dashboardLinks.snaps

--- components/account-dashboard/DashboardLinks/__test__/dashboardLinks.test.js
+++ components/account-dashboard/DashboardLinks/__test__/dashboardLinks.test.js
@@ -5,8 +5,9 @@ import * as nextRouter from "next/router";
 nextRouter.useRouter = jest.fn();
 nextRouter.useRouter.mockImplementation(() => ({
   route: "/",
+  pathname: "/mock/pathname",
 }));
-it("renders a snapshot of <DashboardLinks />", () => {
+it("renders a snapshot of <DashboardLinks /> ", () => {
   const board = renderer.create(<DashboardLinks />).toJSON();
   expect(board).toMatchSnapshot();
 });
