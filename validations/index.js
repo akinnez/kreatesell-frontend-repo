7aa07e3cf3c00ec6bd8176f0f6e134b70348edfb
maxinth <tunde.j.adeleye@gmail.com>
2022-04-06 16:02:39 +0100

    new changes for feedback valiadation

--- validations/index.js
+++ validations/index.js
@@ -4,9 +4,7 @@ export const WaitListSchema = () => {
   return Yup.object().shape({
     // added trim to account for when whitespace is supplied
 
-    customer_name: Yup.string("Please put in your details to proceed")
-      .trim()
-      .required("Please input your first name"),
+    customer_name: Yup.string().trim().required("Please input your first name"),
     customer_email: Yup.string()
       .email("Please use a valid e-mail address")
       .required("Please input your e-mail address"),
