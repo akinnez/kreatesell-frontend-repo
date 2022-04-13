e844ec563de21e2c8a3fe7f70a8caea65cb38b30
maxinth <tunde.j.adeleye@gmail.com>
2022-04-13 11:49:24 +0100

    moved push notification to pages folder

--- components/Attention/Form/index.jsx
+++ components/Attention/Form/index.jsx
@@ -31,7 +31,7 @@ const Form = ({ showSubmissionSuccessModal, showSubmissionFailureModal }) => {
       axios
         .post(waitListUrl, data)
         .then((res) => {
-          console.log(res);
+          console.log(res?.data?.message);
           // DETAILS GO THROUGH, THEN showSubmissionSuccessModal
           showSubmissionSuccessModal();
         })
