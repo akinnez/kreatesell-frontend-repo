6582d3f90edf2e0923507705ab6ca97fc32b868e
maxinth <tunde.j.adeleye@gmail.com>
2022-04-14 11:30:45 +0100

    fixed sneakPeak items margin issue on mobile

---
+++ components/SneakPeak/data.js
@@ -0,0 +1,10 @@
+export const benefits = [
+'Free digital products upload.',
+'21 supported currencies.',
+'Earn massively from borderless sales.',
+'Army of affiliate marketers to sell your digital product(s) for you.',
+'24/7 customer support.',
+'Automated done-for-you payment and follow up processes.',
+'And so much more...',
+
+]
