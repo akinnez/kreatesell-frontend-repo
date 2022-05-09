165d68590b835f02f45ae834f34483f31db55a67
maxinth <tunde.j.adeleye@gmail.com>
2022-05-09 15:14:01 +0100

    added snaps for manageContent component

--- components/products/BusinessSection/__test__/manageContent.test.js
+++ components/products/BusinessSection/__test__/manageContent.test.js
@@ -2,6 +2,6 @@ import renderer from "react-test-renderer";
 import ManageContent from "../ManageContent";
 
 it("renders a snapshot of <ManageContent />", () => {
-  const elem = renderer.create(<ManageContent />).toJSON();
-  expect(elem).toMatchSnapshot();
+  const content = renderer.create(<ManageContent />).toJSON();
+  expect(content).toMatchSnapshot();
 });
