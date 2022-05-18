d7c6632ee5caa010eeeaa1fd7edc92cf66c2653d
tundeLayan <tunderoxyafolayan@gmail.com>
2022-05-18 12:34:13 +0100

    Added appropriate answers for FAQs

--- pages/api/faqs/KreatorAffiliate.js
+++ pages/api/faqs/KreatorAffiliate.js
@@ -1,47 +1,75 @@
 export const questions = [
     {
         question: "How long does it take to receive my money?", 
-        answer:"Settlements are made between 24 hours to 10 days, depending on your currency and country."
+        answer: `On Kreatesell, we provide payout and settlement at due intervals of 24 hours to 10 days depending on your currency and country. Check out your wait time here.
+
+        If payment is not received by the set date, you can report the issue to us at payment@kreatesell.com
+        `
     },
     {
         question: "How do I know if I have made sales?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
-    },
-    {
-        question: "How do I know if I have made sales?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `You get email notifications from Kreatesell for every successful sales transaction.
+
+        You can also monitor how much sales have been made on your digital product (for Kreator) or the product you are promoting (for Affiliate) through the transaction history on your dashboard.
+        `
     },
     {
         question: "Can I sign up with my social media account?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `Yes! You can sign up on Kreatesell with your social media account. After clicking ‘Get Started Free’ or ‘sign up’ to create an account, you can click on the Facebook button to sign up with your Facebook social media account. Also, you can click on the Google button to sign up with your Gmail account.
+        `
     },
     {
         question: "Are my login details safe?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `Yes, Kreatesell provides maximum security on our website. So, your login information is safe on the site. All passwords entered into our website are encrypted so, they are not visible to anyone, including us. We also have the option for you to activate Two Factor Authentication (2FA). If you have issues with logging in to your account, send us an email at business@kreatesell.com with your registered email address.
+        `
     },
     {
         question: "Can I be both an Affiliate and a Kreator on Kreatesell at the same time?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `Yes! Signing up as a user on Kreatesell allows you to be either a Kreator, an affiliate or both.
+
+        If you are an existing Kreator and are interested in being an affiliate for a product listed in the Kreatesell marketplace, all you need to do is request to be an affiliate, once you are approved, you get your referral link and you can start promoting.
+        
+        If you are an affiliate already but want to start selling as a Kreator, just click on Add product, follow the prompts, fill in the required details and you’re good to go.
+        `
     },
     {
         question: "What is the minimum withdrawal amount?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `The payout for Nigerian Kreators is automatic. But for Kreators outside Nigeria, payouts are made immediately you click the withdraw button. You receive the total balance in your wallet at once. Before you can withdraw, you are required to have a minimum balance as follows:
+
+        Nigerian Naira (NGN): NGN 50
+        Ghana Cedi (GHS): GHS 10
+        Kenyan Shilling (KES): KES 100
+        Tanzanian Shilling (TZS): TZS 3500
+        Ugandan Shilling (UGX): UGX 12, 000
+        South African Rands (ZAR): ZAR 12
+        Central African CFA franc (XAF): 600 XAF
+        West African CFA franc (XOF): 600 XOF
+        US Dollars (USD): USD 0 - Free (via PayPal)
+        British Pounds (GBP): GBP 0 - Free (via PayPal)
+        This serves as the withdrawal fees for taking funds out of your wallet.
+        
+        However, as an affiliate, you have to make successful sales and accumulate commissions to a threshold amount of N5,000 or $20 for you to get your first payment. Payouts are made every Tuesday.
+        `
     },
     {
         question: "Why am I yet to receive my settlement/payout?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `On Kreatesell, we provide payout and settlement at due intervals of 24 hours to 10 days depending on your currency and country. Check out your wait time here.
+
+        If payment is not received by the set date, you can report the issue to us at payment@kreatesell.com.
+        `
     },
     {
         question: "Can I cancel my Kreatesell account if I don’t want to use it anymore?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `Yes! To delete your Kreatesell account, click on the HELP menu and fill out the form. You would be promptly attended to by our customer success support team who are always ready to help.
+        `
     },
     {
         question: "If I delete my Kreatesell account, would I lose my data?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `Considering that Kreatesell is a software as a service (SaaS) platform hosted on the web, all data on our platform is stored in the cloud. So, deleting your account means you delete all the information on the cloud and won’t be able to access it. So, we advise that you keep your account in case you would love to access your data in the future.`
     },
     {
         question: "Who owns my information/data/content on Kreatesell?", 
-        answer:"Kreatesell is an easy-to-use, cross-border education technology (edtech) SaaS platform that enables content kreators and digital entrepreneurs to upload their digital products for the fastest and easiest sales. With KreateSell, you can convert your audience into buying customers without going through the stress of learning many sales funnels that may be complex to use. Also, you instantly get paid from anywhere in the world conveniently. See more about how it works. Kreatesell is solving the major problems that Kreators face while selling online including having no means to receive payment in foreign currency. With Kreatesell, this is no longer a barrier because buyers can now pay in their local currency and Kreators can receive the money seamlessly in their local currencies without going through the stress of trying to exchange currencies using third-party platforms. On Kreatesell, you have access to a large number of affiliates ready to promote your product to potential buyers with a preset commission fee of your choice. Also, you do not have to go through the stress of manual delivery of your digital product, your delivery process is automated for you. Your customer receives the product immediately after payment is confirmed."
+        answer: `Every information you have put into Kreatesell totally belongs to you. Be rest assured that Kreatesell does not have any right to your content or information. Kreatesell is just a tool to help you make more massive sales in selling your digital product.`
     },
     
   ]
