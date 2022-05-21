5d3e307ea7c23cb08050ab91ae8648cb92ded132
maxinth <tunde.j.adeleye@gmail.com>
2022-05-21 12:45:02 +0100

    added snaps for Affiliate and buyer tabs as used on the faq page

---
+++ components/faqTabs/__test__/buyer.test.js
@@ -0,0 +1,8 @@
+import { questions } from "../../../pages/api/faqs/Affiliate";
+import renderer from "react-test-renderer";
+import Buyer from "../Buyer";
+
+it("renders a snapshot of <Buyer/> tab", () => {
+  const tab = renderer.create(<Buyer questions={questions} />).toJSON();
+  expect(tab).toMatchSnapshot();
+});
