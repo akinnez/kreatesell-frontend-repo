b5c82ef08707420609b032c8dfdcad7a0e2a4c20
maxinth <tunde.j.adeleye@gmail.com>
2022-05-21 12:52:30 +0100

    updated kreatorAffiliateTab snaps

--- components/faqTabs/__test__/kreatorAffiliate.test.js
+++ components/faqTabs/__test__/kreatorAffiliate.test.js
@@ -2,7 +2,7 @@ import { questions } from "../../../pages/api/faqs/KreatorAffiliate";
 import renderer from "react-test-renderer";
 import KreatorAffiliate from "../KreatorAffiliate";
 
-it("renders a snapshot of <Kreator/> tab", () => {
+it("renders a snapshot of <KreatorAffiliate /> tab", () => {
   const tab = renderer
     .create(<KreatorAffiliate questions={questions} />)
     .toJSON();
