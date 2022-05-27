548de5b561dc21577a6b958483796746560617e0
maxinth <tunde.j.adeleye@gmail.com>
2022-05-27 13:11:39 +0100

    fixed forgot password bug

--- components/forms/ForgotPasswordForm.jsx
+++ components/forms/ForgotPasswordForm.jsx
@@ -9,53 +9,53 @@ import { useSelector } from "react-redux";
 import styles from "../../public/css/ForgotPassword.module.scss";
 
 export const ForgotPasswordForm = () => {
-	const router = useRouter();
-	const initiatePasswordReset = InitiatePasswordReset();
+  const router = useRouter();
+  const initiatePasswordReset = InitiatePasswordReset();
 
-	const { loading } = useSelector((state) => state.auth);
+  const { loading } = useSelector((state) => state.auth);
 
-	const initialValues = {
-		username: "",
-	};
+  const initialValues = {
+    username: "",
+  };
 
-	const handleSubmit = (data) => {
-		initiatePasswordReset(data, () => {
-			router.push("/forgot-password/token");
-		});
-	};
+  const handleSubmit = (data) => {
+    initiatePasswordReset(data, () => {
+      router.push("/forgot-password/token");
+    });
+  };
 
-	const formik = useFormik({
-		initialValues,
-		onSubmit: handleSubmit,
-		validationSchema: ForgotPasswordSchema,
-		validateOnChange: false,
-	});
+  const formik = useFormik({
+    initialValues,
+    onSubmit: handleSubmit,
+    validationSchema: ForgotPasswordSchema,
+    validateOnChange: false,
+  });
 
-	return (
-		<div className={styles.body}>
-			{!isAnEmpytyObject(formik.errors) && <FormError errors={formik.errors} />}
+  return (
+    <div className={styles.body}>
+      {!isAnEmpytyObject(formik.errors) && <FormError errors={formik.errors} />}
 
-			<form
-				onSubmit={formik.handleSubmit}
-				autoComplete="off"
-				className={styles.container}
-			>
-				<Input
-					label="Email or Phone number"
-					name="username"
-					placeholder="Enter your Email or Phone number"
-					onChange={formik.handleChange}
-				/>
+      <form
+        onSubmit={formik.handleSubmit}
+        autoComplete="off"
+        className={styles.container}
+      >
+        <Input
+          label="Email or Phone number"
+          name="username"
+          placeholder="Enter your Email "
+          onChange={formik.handleChange}
+        />
 
-				<Button text="Reset password" bgColor="primaryBlue" loading={loading} />
-			</form>
+        <Button text="Reset password" bgColor="primaryBlue" loading={loading} />
+      </form>
 
-			<div className={styles.footer}>
-				Already have an account?{" "}
-				<Link href="/login">
-					<a>Login here</a>
-				</Link>{" "}
-			</div>
-		</div>
-	);
+      <div className={styles.footer}>
+        Already have an account?{" "}
+        <Link href="/login">
+          <a>Login here</a>
+        </Link>{" "}
+      </div>
+    </div>
+  );
 };
