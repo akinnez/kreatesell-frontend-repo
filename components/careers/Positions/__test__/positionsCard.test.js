78c7f005851c7c72e188fcf83ee1d97f1483908e
maxinth <tunde.j.adeleye@gmail.com>
2022-06-07 11:21:27 +0100

    added snaps for positionscard

---
+++ components/careers/Positions/__test__/positionsCard.test.js
@@ -0,0 +1,14 @@
+import PositionsCard from "../PositionsCard";
+import renderer from "react-test-renderer";
+
+const mockProps = {
+  title: "product designer",
+  description: "mock description",
+  roles: ["graphic designer, product designer"],
+  path: "/",
+};
+
+it("renders a snapshot of <PositionsCard /> with mockprops", () => {
+  const card = renderer.create(<PositionsCard {...mockProps} />).toJSON();
+  expect(card).toMatchSnapshot();
+});
