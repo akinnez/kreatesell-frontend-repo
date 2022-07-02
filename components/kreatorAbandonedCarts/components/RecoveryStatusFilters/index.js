010f84bc0e7fd64653e88f81ca6b41f1ce94fcfb
ade_lana <lanadvictor@yahoo.co.uk>
2022-07-02 04:39:08 +0100

    Recovery Status UI modifications

--- components/kreatorAbandonedCarts/components/RecoveryStatusFilters/index.js
+++ components/kreatorAbandonedCarts/components/RecoveryStatusFilters/index.js
@@ -2,17 +2,9 @@ import { useState } from "react";
 import Image from "next/image";
 import moment from "moment";
 import { Row, Col, Form, Select, DatePicker, Button } from "antd";
-import { MdOutlineCancel } from "react-icons/md";
 import { currencyOptions } from "utils";
 import styles from "./index.module.scss";
-
-const ResetBtn = ({ resetFilters }) => (
-  <div className={styles.resetFilters}>
-    <Button shape="round" icon={<MdOutlineCancel />} onClick={resetFilters}>
-      Clear filters
-    </Button>
-  </div>
-);
+import ResetFilters from "components/ResetFilters";
 
 // const options = [
 //   { label: "Today", value: "today" },
@@ -23,9 +15,14 @@ const ResetBtn = ({ resetFilters }) => (
 
 const currencies = [{ value: "All", label: "All" }, ...currencyOptions];
 
+const breakpoints = {
+  xs: { span: 12 },
+  sm: { span: 12 },
+  md: { span: 6 },
+};
+
 const RecoveryStatusFilters = ({ setFilters }) => {
   const [isFiltered, setIsFiltered] = useState(false);
-  const [showFilter, setShowFilter] = useState(false);
 
   const [form] = Form.useForm();
 
@@ -45,7 +42,6 @@ const RecoveryStatusFilters = ({ setFilters }) => {
     if (!currency && !from && !to) return;
 
     setIsFiltered(true);
-    setShowFilter(false);
     setFilters({
       currency,
       from: from ? from._i : "",
@@ -64,38 +60,17 @@ const RecoveryStatusFilters = ({ setFilters }) => {
   };
 
   return (
-    <div>
-      <div className={styles.filterToggle}>
-        <Button shape="round" onClick={handleClicks(setShowFilter, true)}>
-          Show Filters...
-        </Button>
-        {isFiltered && <ResetBtn resetFilters={resetFilters} />}
-      </div>
-      <div
-        className={
-          showFilter
-            ? `${styles.filtersContainer} ${styles.filtersContainerVisible}`
-            : `${styles.filtersContainer}`
-        }
+    <div className={!isFiltered && styles["lg-margin"]}>
+      <Form
+        labelCol={{ span: 24 }}
+        wrapperCol={{ span: 24 }}
+        onFinish={handleSubmitFilter}
+        size="large"
+        form={form}
+        name="filter_form"
       >
-        <Button
-          shape="circle"
-          type="text"
-          icon={<MdOutlineCancel />}
-          onClick={handleClicks(setShowFilter, false)}
-          className={styles.closeFilter}
-        />
-        <div className={isFiltered ? styles.mdMargin : styles.lgMargin}>
-          <Form
-            labelCol={{ span: 24 }}
-            wrapperCol={{ span: 24 }}
-            onFinish={handleSubmitFilter}
-            size="large"
-            form={form}
-            name="filter_form"
-          >
-            <Row gutter={20} align="bottom" justify="space-between">
-              {/* <Col
+        <Row gutter={[15, 14]} align="bottom" justify="space-between" wrap>
+          {/* <Col
                 xs={{ span: 24 }}
                 lg={{ span: 5 }}
                 className={styles.input__wrapper}
@@ -108,67 +83,52 @@ const RecoveryStatusFilters = ({ setFilters }) => {
                   />
                 </Form.Item>
               </Col> */}
-              <Col
-                xs={{ span: 24 }}
-                lg={{ span: 7 }}
-                className={styles.input__wrapper}
-              >
-                <Form.Item label="Currency" name="currency">
-                  <Select
-                    placeholder="NGN"
-                    onChange={handleOptions("currency")}
-                    options={currencies}
-                  />
-                </Form.Item>
-              </Col>
-              <Col
-                xs={{ span: 24 }}
-                lg={{ span: 7 }}
-                className={styles.input__wrapper}
-              >
-                <Form.Item label="From" name="from">
-                  <DatePicker
-                    placeholder="2021-07-22"
-                    onChange={handleDate("from")}
-                    allowClear={false}
-                  />
-                </Form.Item>
-              </Col>
-              <Col
-                xs={{ span: 24 }}
-                lg={{ span: 7 }}
-                className={styles.input__wrapper}
-              >
-                <Form.Item label="To" name="to">
-                  <DatePicker
-                    placeholder="2021-07-22"
-                    onChange={handleDate("to")}
-                    allowClear={false}
-                  />
-                </Form.Item>
-              </Col>
-              <Col
-                xs={{ span: 24 }}
-                lg={{ span: 3 }}
-                className={styles.filter__btn}
-              >
-                <Form.Item>
-                  <Button type="primary" htmlType="submit">
-                    <Image
-                      src="/images/FilterIcon.png"
-                      alt="Filter icon"
-                      width={19}
-                      height={16}
-                    />
-                    &nbsp; Filter
-                  </Button>
-                </Form.Item>
-              </Col>
-            </Row>
-          </Form>
-        </div>
-      </div>
-      {isFiltered && <ResetBtn resetFilters={resetFilters} />}
+          <Col {...breakpoints} className={styles.input__wrapper}>
+            <Form.Item label="Currency" name="currency">
+              <Select
+                placeholder="NGN"
+                onChange={handleOptions("currency")}
+                options={currencies}
+              />
+            </Form.Item>
+          </Col>
+          <Col {...breakpoints} className={styles.input__wrapper}>
+            <Form.Item label="From" name="from">
+              <DatePicker
+                placeholder="2021-07-22"
+                onChange={handleDate("from")}
+                allowClear={false}
+              />
+            </Form.Item>
+          </Col>
+          <Col {...breakpoints} className={styles.input__wrapper}>
+            <Form.Item label="To" name="to">
+              <DatePicker
+                placeholder="2021-07-22"
+                onChange={handleDate("to")}
+                allowClear={false}
+              />
+            </Form.Item>
+          </Col>
+          <Col
+            {...breakpoints}
+            className={`${styles.input__wrapper} ${styles.filter__btn}`}
+          >
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                <Image
+                  src="/images/FilterIcon.png"
+                  alt="Filter icon"
+                  width={19}
+                  height={16}
+                />
+                &nbsp; Filter
+              </Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      {isFiltered && <ResetFilters resetFilters={resetFilters} />}
     </div>
   );
 };
