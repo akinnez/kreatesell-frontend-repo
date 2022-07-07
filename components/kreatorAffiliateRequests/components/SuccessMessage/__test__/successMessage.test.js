550ffc61e12d9114000dd50fb3a0a94dd9d40ea2
maxinth <tunde.j.adeleye@gmail.com>
2022-07-07 12:53:13 +0100

    added successMessage.snaps

---
+++ components/kreatorAffiliateRequests/components/SuccessMessage/__test__/successMessage.test.js
@@ -0,0 +1,7 @@
+import SuccessMessage from "..";
+import renderer from "react-test-renderer";
+
+it("renders a snapshot of <SuccessMessage />", () => {
+  const message = renderer.create(<SuccessMessage />).toJSON();
+  expect(message).toMatchSnapshot();
+});
