24b3b4e1283e965231f27baca47c9b7d75240bfc
ade_lana <lanadvictor@yahoo.co.uk>
2022-07-11 09:38:09 +0100

    ui updates for affiliate product preview

--- components/affiliateProductPreview/ProductKreator/index.js
+++ components/affiliateProductPreview/ProductKreator/index.js
@@ -3,25 +3,23 @@ import KreatorInfo from "components/affiliates/KreatorInfo";
 import KreatorAvatar from "components/affiliates/KreatorAvatar";
 import styles from "./index.module.scss";
 
-const ProductKreator = ({ kreatorImage, kreatorName, storeName }) => {
-  return (
-    <div className={styles.product__kreator}>
-      {kreatorImage ? (
-        <div className={styles.product__kreator__avatar}>
-          <KreatorAvatar image={kreatorImage} name={kreatorName} />
-        </div>
-      ) : (
-        <div className={styles.empty__avatar}>
-          <FaRegUser />
-        </div>
-      )}
-      <div className={styles.product__kreator__info}>
-        <KreatorInfo href={storeName}>
-          <p>{kreatorName}</p>
-        </KreatorInfo>
+const ProductKreator = ({ kreatorImage, kreatorName, storeName }) => (
+  <div className={styles.product__kreator}>
+    {kreatorImage ? (
+      <div className={styles.product__kreator__avatar}>
+        <KreatorAvatar image={kreatorImage} name={kreatorName} />
       </div>
+    ) : (
+      <div className={styles.empty__avatar}>
+        <FaRegUser />
+      </div>
+    )}
+    <div className={styles.product__kreator__info}>
+      <KreatorInfo href={storeName}>
+        <p>{kreatorName}</p>
+      </KreatorInfo>
     </div>
-  );
-};
+  </div>
+);
 
 export default ProductKreator;
