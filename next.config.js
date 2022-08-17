f24e0ba9f2a55c1ae06ffae3079e05094831f54d
tundeLayan <tunderoxyafolayan@gmail.com>
2022-08-17 12:02:15 +0100

    Config files

--- next.config.js
+++ next.config.js
@@ -1,18 +1,34 @@
 module.exports = {
   env: {
-    BASE_URL: "https://kreatesell.io/api/",
-    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: "6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB",
-    DISQUS_SHORTNAME: "kreatesell-com",
+    BASE_URL: 'https://kreatesell.io/api/',
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB',
+    DISQUS_SHORTNAME: 'kreatesell-com',
     FB_APP_ID: 429932695011083,
-    GOOGLE_CLIENT_KEY: "1056121378929-mvsh5rqbqp4mq6jaktlull3jhs6rtevu.apps.googleusercontent.com",
+    GOOGLE_CLIENT_KEY:
+      '1056121378929-mvsh5rqbqp4mq6jaktlull3jhs6rtevu.apps.googleusercontent.com',
     DISQUS_PUBLIC_KEY:
-      "6lSQoKFGTpA9fercSGt0klM60BCv7vgF2PMnPb1NqNhpo6HTmwRpkRfAs4VVMLFp",
-      NEXT_PUBLIC_RECAPTCHA_SITE_KEY:"6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB",
-      NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY:"pk_test_1507a32eacc6e969e3c63d30052f77e12f29f78f",
-      NEXT_PUBLIC_PAYSTACK_GHANA_PUBLIC_KEY:"pk_test_25cc61cc04df35fe9e3e09f4a7b830b8050b7d89",
-      NEXT_PUBLIC_FLUTTERWAVE_PUBLIC_KEY :"FLWPUBK_TEST-3f92e2c7e96647a15cfd7206f88dcd60-X"
+      '6lSQoKFGTpA9fercSGt0klM60BCv7vgF2PMnPb1NqNhpo6HTmwRpkRfAs4VVMLFp',
+    NEXT_PUBLIC_RECAPTCHA_SITE_KEY: '6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB',
+    NEXT_PUBLIC_PAYSTACK_PUBLIC_KEY:
+      'pk_test_1507a32eacc6e969e3c63d30052f77e12f29f78f',
+    NEXT_PUBLIC_PAYSTACK_GHANA_PUBLIC_KEY:
+      'pk_test_25cc61cc04df35fe9e3e09f4a7b830b8050b7d89',
+    NEXT_PUBLIC_FLUTTERWAVE_PUBLIC_KEY:
+      'FLWPUBK_TEST-3f92e2c7e96647a15cfd7206f88dcd60-X',
+    // NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: 'pk_live_51KHkOkEiRwUsDs0AlNvA7bynwKfMvaj7rdfGpejucsZalNh7KwlWe3RcUizNoYk16yvJ9MSgcALp5SzUFAyFAA3g00OnWxz6eA',
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY:
+      'pk_test_51KHkOkEiRwUsDs0AJ15ufHXXYL7Zbs26ZpKiqiSuWjnjeDtYkMaMPAdL4eEBVwQtzrZs12abo0UxtNdNzXFiywsQ005dNpADpb',
+    NEXT_PUBLIC_PAYPAL_PUBLISHABLE_KEY:
+      'AW7u10uJYBenaA-igO_LE3kZFljEyvqIQtl_gI7NM8bsl8ItY6m4g8mHaYl41FJHgvjZiJ2RnoOQkKcf',
   },
   images: {
-    domains: ["res.cloudinary.com", "flagcdn.com", "upload.wikimedia.org","www.bellanaija.com","i.pinimg.com" ,""],
+    domains: [
+      'res.cloudinary.com',
+      'flagcdn.com',
+      'upload.wikimedia.org',
+      'www.bellanaija.com',
+      'i.pinimg.com',
+      '',
+    ],
   },
-};
+}
