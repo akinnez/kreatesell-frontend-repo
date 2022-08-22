9cd815622bb2a58bf2c5fe194ef41dcac0e4e4c0
tundeLayan <tunderoxyafolayan@gmail.com>
2022-08-22 14:56:23 +0100

    fixed build error

--- pages/api/create-payment-intent.js
+++ pages/api/create-payment-intent.js
@@ -9,18 +9,16 @@ const calculateOrderAmount = (items) => {
 }
 
 export default async function handler(req, res) {
-  const { items, type, price, currency } = req.body
-
-  // Create a PaymentIntent with the order amount and currency
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: type === 'subscription' ? price : typecalculateOrderAmount(items),
-    currency: 'eur',
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  })
-
-  res.send({
-    clientSecret: paymentIntent.client_secret,
-  })
+  // const { items, type, price, currency } = req.body
+  // // Create a PaymentIntent with the order amount and currency
+  // const paymentIntent = await stripe.paymentIntents.create({
+  //   amount: type === 'subscription' ? price : typecalculateOrderAmount(items),
+  //   currency: 'eur',
+  //   automatic_payment_methods: {
+  //     enabled: true,
+  //   },
+  // })
+  // res.send({
+  //   clientSecret: paymentIntent.client_secret,
+  // })
 }
