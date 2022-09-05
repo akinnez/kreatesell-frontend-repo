ecc6eda5bdf2eabae646104d992df6217187ad08
tundeLayan <tunderoxyafolayan@gmail.com>
2022-09-05 11:54:57 +0100

    Clean up

--- components/forms/UpgradeAccountForm.jsx
+++ components/forms/UpgradeAccountForm.jsx
@@ -15,13 +15,12 @@ import { SendPaymentCheckoutDetails } from 'redux/actions'
 import { Button } from 'components/button/Button'
 import {
   ActiveTick,
-  InactiveMasterCard,
-  InactivePaypal,
   ActiveStripe,
   AdvancedBitcoin,
   AdvancedPaypal,
   AdvancedStripe,
   splitFullName,
+  FlutterwaveLogo,
 } from 'utils'
 import { RightArrow } from 'utils/icons/RightArrow'
 import useCurrency from 'hooks/useCurrency'
@@ -41,6 +40,11 @@ const paymentMethods = [
     type: 'Paypal',
     icon: AdvancedPaypal,
     value: 'paypal',
+  },
+  {
+    type: 'Flutterwave',
+    icon: FlutterwaveLogo,
+    value: 'flutterwave',
   },
   {
     type: 'CryptoCurrency',
@@ -255,12 +259,15 @@ export const UpgradeAccountForm = ({
     // currencies using stripe
 
     /** Currencies using FlutterWave are listed here. When other payment options for USD and GBP are implemented, remember to consider it here also */
-    if (!['NGN', 'GHS', 'GBP', 'USD'].includes(activeCurrency.currency)) {
+    if (
+      (!['NGN', 'GHS'].includes(activeCurrency.currency) ||
+        selectedPaymentMethod === 'flutterwave') &&
+      !['paypal', 'stripe', 'crypto'].includes(selectedPaymentMethod)
+    ) {
       setModal(false)
-      // console.log('I got here')
       handleFlutterPayment({
         callback: async (response) => {
-          console.log('response ', response)
+          // console.log('response ', response)
           await sendPaymentCheckoutDetails(
             paymentDetails({
               reference: response?.tx_ref,
@@ -314,157 +321,8 @@ export const UpgradeAccountForm = ({
     setSelectedPaymentMethod(method)
   }
 
-  // const handlePayment = (e) => {
-  //   e.preventDefault()
-  //   if (!['USD', 'GBP'].includes(activeCurrency.currency)) {
-  //     // console.log('I got here')
-  //     setSelectedPaymentMethod(() => '')
-  //   }
-  //   const data = {
-  //     fullname: 'string',
-  //     email_address: 'string',
-  //     mobile_number: 'string',
-  //     datetime: '2022-08-08T14:24:41.326Z',
-  //     total: 0,
-  //     reference_id: 'string',
-  //     purchase_details: [
-  //       {
-  //         product_id: 0,
-  //         quantity: 0,
-  //         amount: 0,
-  //       },
-  //     ],
-  //     status: 'string',
-  //     card_type: 'string',
-  //     last_four: 'string',
-  //     currency: 'string',
-  //     is_affiliate: true,
-  //     affiliate_product_link: 'string',
-  //   }
-  //   console.log(
-  //     'activeCurrency: ',
-  //     activeCurrency,
-  //     '\nselectedPaymentMethod',
-  //     selectedPaymentMethod,
-  //   )
-  //   // backend enpoint
-  //   // makePlanUpgrade(data, ()=>console.log("success"), ()=>console.log("error"));
-  // }
-
   if (loading) return <Loader />
 
-  // TODO: if selected payment method is stripe
-  // if (selectedPaymentMethod === 'stripe')
-  //   return (
-  //     <>
-  //       {clientSecret && (
-  //         <Elements options={options} stripe={stripePromise}>
-  //           <PaymentElement id="payment-element" />
-  //           <div className="px-0 md:px-5">
-  //             <div className="text-center mb-4">
-  //               <h3 className="text-black-100 font-bold text-xl">
-  //                 Upgrade Your Account
-  //               </h3>
-  //               <h4 className="text-black-100 pt-2">BUSINESS</h4>
-  //               <div className="divider"></div>
-
-  //               <div className="text-base-green-200 font-bold text-2xl">
-  //                 <sup className="font-normal text-xs text-black-100">NGN</sup>{' '}
-  //                 4,167
-  //                 <sub className="font-normal text-xs text-black-100">
-  //                   / Month
-  //                 </sub>
-  //               </div>
-  //             </div>
-
-  //             <form className="px-2 md:px-2 pt-4" onSubmit={handleSubmitStripe}>
-  //               <div className="text-primary-blue font-medium text-lg">
-  //                 Payment Details
-  //               </div>
-  //               <div className="divider"></div>
-
-  //               <div>
-  //                 <div>Select Currency</div>
-  //                 <p className="text-base-gray-200 text-xs pt-2 md:pt-0 md:text-sm">
-  //                   Select your preferred currency and get price equivalent
-  //                 </p>
-  //               </div>
-  //               <div className="grid gap-4 grid-cols-3 md:grid-cols-6 pt-3">
-  //                 {countriesCurrency?.map(
-  //                   ({ currency, currency_id, flag }, i) => (
-  //                     <CurrencyCard
-  //                       key={currency_id}
-  //                       handleSelect={() =>
-  //                         handleSelect({ currency_id, currency })
-  //                       }
-  //                       {...{ currency, currency_id, flag, activeCurrency }}
-  //                     />
-  //                   ),
-  //                 )}
-  //               </div>
-
-  //               <div className="pt-6">
-  //                 <div>Payment Method</div>
-  //                 <p className="text-base-gray-200 text-xs pt-2 md:pt-0 md:text-sm">
-  //                   Select your preferred payment method
-  //                 </p>
-  //               </div>
-  //               {/* paystack is NGN and GHS */}
-
-  //               {/* only show this section if selected currency is "USD" or "GBP" */}
-  //               {['USD', 'GBP'].includes(activeCurrency?.currency) && (
-  //                 <div className="grid gap-4 grid-cols-3 pt-3">
-  //                   {paymentMethods.map(({ type, icon, value }) => (
-  //                     <div
-  //                       key={value}
-  //                       onClick={() => handlePaymentMethod(value)}
-  //                       className={`${
-  //                         selectedPaymentMethod === value
-  //                           ? 'activeCard'
-  //                           : 'card'
-  //                       } p-2 flex justify-around items-center`}
-  //                     >
-  //                       <Image src={icon} alt={type} />
-  //                       {selectedPaymentMethod === value && (
-  //                         <Image
-  //                           src={ActiveTick}
-  //                           alt="active"
-  //                           width="16"
-  //                           height="16"
-  //                         />
-  //                       )}
-  //                     </div>
-  //                   ))}
-  //                 </div>
-  //               )}
-
-  //               <div className="priceMenu my-6 py-3 px-8">
-  //                 <div className="flex justify-between pt-2">
-  //                   <p>SubTotal</p>
-  //                   <p>NGN 4,167</p>
-  //                 </div>
-  //                 <div className="divider"> </div>
-  //                 <div className="flex justify-between">
-  //                   <p>Total</p>
-  //                   <p className="text-primary-blue font-medium">NGN 4,167</p>
-  //                 </div>
-  //               </div>
-
-  //               <div className="w-full">
-  //                 <Button
-  //                   disabled={isLoading || !stripe || !elements}
-  //                   text="Pay NGN 4,167"
-  //                   bgColor="blue"
-  //                   style={{ width: '100%' }}
-  //                   icon={<RightArrow />}
-  //                 />
-  //               </div>
-  //             </form>
-  //           </div>
-  //         </Elements>
-  //       )}
-  //     </>
-  //   )
   return (
     <>
       <div className="px-0 md:px-5">
