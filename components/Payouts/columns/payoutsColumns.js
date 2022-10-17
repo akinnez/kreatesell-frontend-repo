7b7eefc63689f05ac26f12afc2d6ec54a2b5124b
tundeLayan <tunderoxyafolayan@gmail.com>
2022-10-17 10:41:05 +0100

    Payout columns

--- components/Payouts/columns/payoutsColumns.js
+++ components/Payouts/columns/payoutsColumns.js
@@ -1,22 +1,22 @@
-import dateFormat from "utils/dateFormat";
-import formatNumber from "utils/formatNumber";
+import dateFormat from 'utils/dateFormat'
+import formatNumber from 'utils/formatNumber'
 
 export const payoutsColumns = [
-  { title: "Product", dataIndex: "product_name" },
-  { title: "Customer Name", dataIndex: "customer_name" },
-  { title: "Customer Email", dataIndex: "customer_email" },
+  { title: 'Product', dataIndex: 'product' },
+  { title: 'Customer Name', dataIndex: 'customer_fullname' },
+  { title: 'Customer Email', dataIndex: 'customer_email_address' },
   {
-    title: "Amount",
-    render: record => `${record.currency} ${formatNumber(record.amount)}`,
+    title: 'Amount',
+    render: (record) => `${record.currency} ${formatNumber(record.amount)}`,
   },
   {
-    title: "Transaction Date",
-    dataIndex: "transaction_date",
-    render: transactionDate => dateFormat(transactionDate),
+    title: 'Transaction Date',
+    dataIndex: 'transaction_date',
+    render: (transactionDate) => dateFormat(transactionDate),
   },
   {
-    title: "Settlement Date",
-    dataIndex: "settlement_date",
-    render: settlementDate => dateFormat(settlementDate),
+    title: 'Settlement Date',
+    dataIndex: 'settlement_date',
+    render: (settlementDate) => dateFormat(settlementDate),
   },
-];
+]
