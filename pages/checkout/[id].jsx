03516862b7a26e37ba6c67d56e1b01bb98413bd1
tundeLayan <tunderoxyafolayan@gmail.com>
2022-11-18 11:09:58 +0100

    linter

--- pages/checkout/[id].jsx
+++ pages/checkout/[id].jsx
@@ -239,16 +239,11 @@ const Checkout = () => {
 
 	const [{options}, dispatch] = usePayPalScriptReducer();
 
-	const {
-		countriesCurrency,
-		filterdWest,
-		filteredCentral,
-	} = useCheckoutCurrency();
+	const {countriesCurrency, filterdWest, filteredCentral} =
+		useCheckoutCurrency();
 
-	const [
-		storecheckoutCurrencyLoading,
-		setStorecheckoutCurrencyLoading,
-	] = useState(true);
+	const [storecheckoutCurrencyLoading, setStorecheckoutCurrencyLoading] =
+		useState(true);
 	const [activeCurrency, setActiveCurrency] = useState({});
 	const [desiredAmount, setDesiredAmount] = useState('');
 
@@ -730,8 +725,7 @@ const Checkout = () => {
 		customizations: {
 			title: 'Kreatesell Title',
 			description: 'Kreatesell description',
-			logo:
-				'https://res.cloudinary.com/salvoagency/image/upload/v1636216109/kreatesell/mailimages/KreateLogo_sirrou.png',
+			logo: 'https://res.cloudinary.com/salvoagency/image/upload/v1636216109/kreatesell/mailimages/KreateLogo_sirrou.png',
 		},
 	};
 
@@ -1331,27 +1325,29 @@ const Checkout = () => {
 													) => {
 														return actions.order.create(
 															{
-																purchase_units: [
-																	{
-																		description:
-																			'customDescription',
-																		amount: {
-																			// value: Number(
-																			// 	convertedPrice
-																			// ).toFixed(2),
-																			value: Number(
-																				getCurrency(
-																					'price'
-																				)
-																			).toFixed(
-																				2
-																			),
-																			currency: getCurrency(
-																				'currency'
-																			),
+																purchase_units:
+																	[
+																		{
+																			description:
+																				'customDescription',
+																			amount: {
+																				// value: Number(
+																				// 	convertedPrice
+																				// ).toFixed(2),
+																				value: Number(
+																					getCurrency(
+																						'price'
+																					)
+																				).toFixed(
+																					2
+																				),
+																				currency:
+																					getCurrency(
+																						'currency'
+																					),
+																			},
 																		},
-																	},
-																],
+																	],
 															}
 														);
 													}}
