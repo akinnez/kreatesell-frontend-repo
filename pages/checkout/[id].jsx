3ac223387a49d2b6c3146ece6775d452a30d69be
tundeLayan <tunderoxyafolayan@gmail.com>
2022-12-06 15:27:28 +0100

    linter

--- pages/checkout/[id].jsx
+++ pages/checkout/[id].jsx
@@ -300,16 +300,11 @@ const Checkout = () => {
 
 	const [{options}, dispatch] = usePayPalScriptReducer();
 
-	const {
-		countriesCurrency,
-		filterdWest,
-		filteredCentral,
-	} = useCheckoutCurrency();
+	const {countriesCurrency, filterdWest, filteredCentral} =
+		useCheckoutCurrency();
 
-	const [
-		storecheckoutCurrencyLoading,
-		setStorecheckoutCurrencyLoading,
-	] = useState(true);
+	const [storecheckoutCurrencyLoading, setStorecheckoutCurrencyLoading] =
+		useState(true);
 	const [activeCurrency, setActiveCurrency] = useState({});
 	const [desiredAmount, setDesiredAmount] = useState('');
 
@@ -430,9 +425,8 @@ const Checkout = () => {
 		return affliateRef;
 	};
 
-	const affiliateUniqueKey = pathName.localStorage?.getItem(
-		'affiliateUniqueKey'
-	);
+	const affiliateUniqueKey =
+		pathName.localStorage?.getItem('affiliateUniqueKey');
 	const getAffiliateUniqueKey = () => {
 		return affiliateUniqueKey;
 	};
@@ -729,10 +723,11 @@ const Checkout = () => {
 					'https://kreatesell.io/api/v1/kreatesell/payment/coinbase-charge',
 					{
 						name: storeDetails?.product_details?.product_name,
-						description: storeDetails?.product_details?.product_description.substring(
-							0,
-							199
-						),
+						description:
+							storeDetails?.product_details?.product_description.substring(
+								0,
+								199
+							),
 						pricing_type: 'fixed_price',
 						local_price: {
 							amount: getCurrency('price'),
@@ -854,8 +849,7 @@ const Checkout = () => {
 		customizations: {
 			title: 'Kreatesell Title',
 			description: 'Kreatesell description',
-			logo:
-				'https://res.cloudinary.com/salvoagency/image/upload/v1636216109/kreatesell/mailimages/KreateLogo_sirrou.png',
+			logo: 'https://res.cloudinary.com/salvoagency/image/upload/v1636216109/kreatesell/mailimages/KreateLogo_sirrou.png',
 		},
 	};
 
@@ -1462,27 +1456,29 @@ const Checkout = () => {
 													) => {
 														return actions.order.create(
 															{
-																purchase_units: [
-																	{
-																		description:
-																			'customDescription',
-																		amount: {
-																			// value: Number(
-																			// 	convertedPrice
-																			// ).toFixed(2),
-																			value: Number(
-																				getCurrency(
-																					'price'
-																				)
-																			).toFixed(
-																				2
-																			),
-																			currency: getCurrency(
-																				'currency'
-																			),
+																purchase_units:
+																	[
+																		{
+																			description:
+																				'customDescription',
+																			amount: {
+																				// value: Number(
+																				// 	convertedPrice
+																				// ).toFixed(2),
+																				value: Number(
+																					getCurrency(
+																						'price'
+																					)
+																				).toFixed(
+																					2
+																				),
+																				currency:
+																					getCurrency(
+																						'currency'
+																					),
+																			},
 																		},
-																	},
-																],
+																	],
 															}
 														);
 													}}
