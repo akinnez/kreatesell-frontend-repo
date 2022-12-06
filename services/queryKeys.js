ad983fbb9cb38139b9fe7ff921f0144712264bfd
tundeLayan <tunderoxyafolayan@gmail.com>
2022-12-06 15:00:46 +0100

    Chore: Help page filters

--- services/queryKeys.js
+++ services/queryKeys.js
@@ -1,6 +1,10 @@
+import {getQueryKeys} from '../utils';
+
 /**
  * @description query keys for the swr instance
  */
 
 export const GET_ACTIVE_BLOGS = `GET_ACTIVE_BLOGS`;
 export const GET_UPGRADE_PLAN_PRICES = `GET_UPGRADE_PLAN_PRICES`;
+
+export const HELP_TICKETS = getQueryKeys('HELP_TICKETS');
