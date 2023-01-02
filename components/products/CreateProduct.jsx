709c004aa495bbef7536f1996c09c8a6289b4b3b
maxinth <tunde.j.adeleye@gmail.com>
2023-01-02 09:06:52 +0100

    fixed DT-18

--- components/products/CreateProduct.jsx
+++ components/products/CreateProduct.jsx
@@ -391,75 +391,75 @@ export const CreateProductTab = ({
 						{...{productId}}
 					/>
 				)}
-				{isBank && (
-					<Modal
-						title={null}
-						footer={null}
-						visible={isBank}
-						onCancel={() => {
-							setIsBank(false);
-							router.push('all');
-						}}
-						// maskClosable={false}
-						closeIcon={<CloseIcon />}
-					>
-						<div className="mt-4 mx-auto w-full py-5 px-2">
-							<h2 className="mb-4 text-lg text-center font-semibold">
-								Set Up Bank Details
-							</h2>
-							<p className="text-base-gray-300 text-center text-sm">
-								In order to start accepting payments from your
-								sales as a Kreator or/and commissions as an
-								Affiliate, kindly setup your bank account,
-								mobile money wallet or PayPal address.
-							</p>
-							<div className="flex justify-between">
-								<div
-									className="border-r-2 border-gray-300"
-									style={{
-										width: '55%',
-										height: '250px',
-										position: 'relative',
-									}}
-								>
-									<Image
-										src={KreatorBankSetting}
-										alt="kreator"
-										layout="fill"
-									/>
-								</div>
-								<div
-									style={{
-										width: '40%',
-										height: '220px',
-										position: 'relative',
-									}}
-								>
-									<Image
-										src={AffilateBankSetting}
-										alt="affilate"
-										layout="fill"
-									/>
-								</div>
+				{/* {isBank && ( */}
+				<Modal
+					title={null}
+					footer={null}
+					visible={isBank}
+					onCancel={() => {
+						setIsBank(false);
+						router.push('all');
+					}}
+					// maskClosable={false}
+					closeIcon={<CloseIcon />}
+				>
+					<div className="mt-4 mx-auto w-full py-5 px-2">
+						<h2 className="mb-4 text-lg text-center font-semibold">
+							Set Up Bank Details
+						</h2>
+						<p className="text-base-gray-300 text-center text-sm">
+							In order to start accepting payments from your sales
+							as a Kreator or/and commissions as an Affiliate,
+							kindly setup your bank account, mobile money wallet
+							or PayPal address.
+						</p>
+						<div className="flex justify-between">
+							<div
+								className="border-r-2 border-gray-300"
+								style={{
+									width: '55%',
+									height: '250px',
+									position: 'relative',
+								}}
+							>
+								<Image
+									src={KreatorBankSetting}
+									alt="kreator"
+									layout="fill"
+								/>
 							</div>
 							<div
-								className={
-									styles.mdBtn +
-									' w-1/2 flex items-center justify-center mx-auto mt-3'
-								}
+								style={{
+									width: '40%',
+									height: '220px',
+									position: 'relative',
+								}}
 							>
-								<Link
-									href={{
-										pathname:
-											'/account/sales/payouts/set-up-bank-details',
-									}}
-								>
-									Setup Account
-								</Link>
+								<Image
+									src={AffilateBankSetting}
+									alt="affilate"
+									layout="fill"
+								/>
 							</div>
 						</div>
-					</Modal>
-				)}
+						<div
+							className={
+								styles.mdBtn +
+								' w-1/2 flex items-center justify-center mx-auto mt-3'
+							}
+						>
+							<Link
+								href={{
+									pathname:
+										'/account/sales/payouts/set-up-bank-details',
+								}}
+							>
+								Setup Account
+							</Link>
+						</div>
+					</div>
+				</Modal>
+				{/* // )} */}
 			</div>
 		</div>
 	);
