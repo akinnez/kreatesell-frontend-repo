262e07ef784e3a1172bd7a91eab4005ee4c80a48
tundeLayan <tunderoxyafolayan@gmail.com>
2023-03-03 11:31:00 +0100

    Feat: Disclaimer message to the footer of store front

--- components/PoweredByKs/index.js
+++ components/PoweredByKs/index.js
@@ -1,8 +1,8 @@
-import {SuccessKreatesellLogo} from 'utils';
+import {RenderIf, SuccessKreatesellLogo} from 'utils';
 import Image from 'next/image';
 import styles from './poweredByKs.module.scss';
 
-export const PoweredByKS = () => {
+export const PoweredByKS = ({showDisclaimer = false, storename = ''}) => {
 	return (
 		<footer className={styles.PoweredByKS}>
 			<p>
@@ -12,6 +12,16 @@ export const PoweredByKS = () => {
 					alt="powered by KreateSell"
 				/>
 			</p>
+			<RenderIf condition={showDisclaimer}>
+				<p className={styles.disclaimer}>
+					<span>DISCLAIMER NOTICE: </span>
+					This store has no relationship with Facebook, Google or any
+					of the advertising platforms you may have seen our ads on.
+					Additionally, this site is NOT endorsed by Facebook, Google
+					or any advertising platforms in any way. This website and
+					all of it&apos;s contents is a property of {storename}.
+				</p>
+			</RenderIf>
 		</footer>
 	);
 };
