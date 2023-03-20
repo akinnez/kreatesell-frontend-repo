71ef60e61ea97fc6b32f2f9ebe624fa433494344
tundeLayan <tunderoxyafolayan@gmail.com>
2023-03-20 12:40:26 +0100

    Chore: Revert base url for dev back to kreatesell.io

--- next.config.js
+++ next.config.js
@@ -16,8 +16,8 @@ module.exports = (phase) => {
 	const env = {
 		isDev: isDev,
 		isProd: isProd,
-		// BASE_URL: 'https://kreatesell.io/api/',
-		BASE_URL: 'https://kreatesell-pilot.azurewebsites.net/api/',
+		BASE_URL: 'https://kreatesell.io/api/',
+		// BASE_URL: 'https://kreatesell-pilot.azurewebsites.net/api/',
 		NEXT_PUBLIC_RECAPTCHA_SITE_KEY:
 			'6Le7-jQcAAAAAOHkoLvhdgAjcmfi2gcHjLKkCzYB',
 		DISQUS_SHORTNAME: 'kreatesell-com',
