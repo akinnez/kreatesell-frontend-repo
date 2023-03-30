f14f1ad321186bcfcf7915ee39bcea8978bef412
Samuel <samuel@kreatesell.com>
2023-03-30 13:54:23 +0100

    feat-get-response-intergration

--- pages/WaitlistModal/WaitlistModal.jsx
+++ pages/WaitlistModal/WaitlistModal.jsx
@@ -6,7 +6,7 @@ const WaitlistModal = ({ setShowModal, showSubmissionSuccessModal, showSubmissio
         <>
             <div className="fixed inset-0 z-50 flex items-center justify-center">
                 <div className="absolute inset-0 bg-black opacity-50" onClick={() => setShowModal(false)}></div>
-                <div className="relative z-10 bg-white px-8 py-6 md:py-8 rounded-lg shadow-lg">
+                <div className="relative z-10 bg-white px-8 py-6 md:py-8 rounded-lg shadow-lg w-11/12 md:w-3/4">
                     <div className='mb-4 py-3 text-center'>
                         <h2 className="text-lg md:text-xl font-bold text-blue-600 mb-5">Get Early Access Now!</h2>
                         <Form
