6720a1f93909bf21ac1fd93bd17ecd1dc8ffa516
tundeLayan <olatunde@kreatesell.com>
2023-04-12 21:33:27 +0100

    Chore: Removed sendpulse modal

--- pages/_app.js
+++ pages/_app.js
@@ -42,11 +42,11 @@ function MyApp({Component, pageProps}) {
 		<Provider store={store}>
 			<Metas />
 			<SalesPageProvider>
-				<Script
+				{/* <Script
 					charset="UTF-8"
 					src="//web.webpushs.com/js/push/723b749315f187ddc541ac9a201d2dd2_1.js"
 					async
-				/>
+				/> */}
 				<ChatScript />
 				<PayPalScriptProvider
 					options={{
