5e38042a317906f38e702f9f6070cf0ba74b5ca4
tundeLayan <olatunde@kreatesell.com>
2023-04-20 20:35:41 +0100

    Linter

--- pages/checkout/[id].jsx
+++ pages/checkout/[id].jsx
@@ -89,16 +89,11 @@ const Checkout = () => {
 	const {countries} = useSelector((state) => state.utils);
 	const [defaultCurrency, setDefaultCurrency] = useState('');
 
-	const {
-		countriesCurrency,
-		filterdWest,
-		filteredCentral,
-	} = useCheckoutCurrency();
+	const {countriesCurrency, filterdWest, filteredCentral} =
+		useCheckoutCurrency();
 
-	const [
-		storecheckoutCurrencyLoading,
-		setStorecheckoutCurrencyLoading,
-	] = useState(true);
+	const [storecheckoutCurrencyLoading, setStorecheckoutCurrencyLoading] =
+		useState(true);
 	const [activeCurrency, setActiveCurrency] = useState({});
 	const [desiredAmount, setDesiredAmount] = useState('');
 
@@ -551,10 +546,11 @@ const Checkout = () => {
 					'https://kreatesell.io/api/v1/kreatesell/payment/coinbase-charge',
 					{
 						name: storeDetails?.product_details?.product_name,
-						description: storeDetails?.product_details?.product_description.substring(
-							0,
-							199
-						),
+						description:
+							storeDetails?.product_details?.product_description.substring(
+								0,
+								199
+							),
 						pricing_type: 'fixed_price',
 						local_price: {
 							amount: getCurrency('price'),
@@ -667,8 +663,7 @@ const Checkout = () => {
 		customizations: {
 			title: storeDetails?.product_details?.product_name || '',
 			description: 'Kreatesell description',
-			logo:
-				'https://res.cloudinary.com/salvoagency/image/upload/v1636216109/kreatesell/mailimages/KreateLogo_sirrou.png',
+			logo: 'https://res.cloudinary.com/salvoagency/image/upload/v1636216109/kreatesell/mailimages/KreateLogo_sirrou.png',
 		},
 	};
 
@@ -1335,8 +1330,7 @@ const Checkout = () => {
 												<div>
 													<PayPalButtons
 														style={{
-															layout:
-																'horizontal',
+															layout: 'horizontal',
 															label: 'pay',
 														}}
 														disabled={
@@ -1356,27 +1350,29 @@ const Checkout = () => {
 														) => {
 															return actions.order.create(
 																{
-																	purchase_units: [
-																		{
-																			description:
-																				'customDescription',
-																			amount: {
-																				// value: Number(
-																				// 	convertedPrice
-																				// ).toFixed(2),
-																				value: Number(
-																					getCurrency(
-																						'price'
-																					)
-																				).toFixed(
-																					2
-																				),
-																				currency: getCurrency(
-																					'currency'
-																				),
+																	purchase_units:
+																		[
+																			{
+																				description:
+																					'customDescription',
+																				amount: {
+																					// value: Number(
+																					// 	convertedPrice
+																					// ).toFixed(2),
+																					value: Number(
+																						getCurrency(
+																							'price'
+																						)
+																					).toFixed(
+																						2
+																					),
+																					currency:
+																						getCurrency(
+																							'currency'
+																						),
+																				},
 																			},
-																		},
-																	],
+																		],
 																}
 															);
 														}}
@@ -1484,8 +1480,7 @@ const Checkout = () => {
 															style={{
 																fontSize:
 																	'15px',
-																color:
-																	'#8C8C8C',
+																color: '#8C8C8C',
 																textDecoration:
 																	'line-through',
 															}}
