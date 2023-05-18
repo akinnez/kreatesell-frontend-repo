e5bb02a8e77f4ffab47fb5cd56078b3b377f64c4
MAXDRIVE <samuel@kreatesell.com>
2023-05-18 12:35:09 +0100

    feat-hubspot-crm-intergration

--- components/ChatWidgetScript/index.jsx
+++ components/ChatWidgetScript/index.jsx
@@ -14,21 +14,20 @@ const ChatScript = () => {
 
 export default ChatScript;
 
-
 // this is tawkto implementattion
 // 		<Script
-		// 			strategy="afterInteractive"
-		// 			dangerouslySetInnerHTML={{
-		// 				__html: `
-		// 				var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-		// 				(function(){
-		// 				var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-		// 				s1.async=true;
-		// 				s1.src='https://embed.tawk.to/635fd218daff0e1306d4ea11/1ggn4pcnt';
-		// 				s1.charset='UTF-8';
-		// 				s1.setAttribute('crossorigin','*');
-		// 				s0.parentNode.insertBefore(s1,s0);
-		// 				})();
-		//   `,
-		// 			}}
-		// 		/>
+// 			strategy="afterInteractive"
+// 			dangerouslySetInnerHTML={{
+// 				__html: `
+// 				var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+// 				(function(){
+// 				var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+// 				s1.async=true;
+// 				s1.src='https://embed.tawk.to/635fd218daff0e1306d4ea11/1ggn4pcnt';
+// 				s1.charset='UTF-8';
+// 				s1.setAttribute('crossorigin','*');
+// 				s0.parentNode.insertBefore(s1,s0);
+// 				})();
+//   `,
+// 			}}
+// 		/>
