2eaf85742b220a453794d6c2a54f7ea79f5aebbc
akinnez <oyetunjiidris@gmail.com>
2023-07-14 11:52:52 +0200

    Get Started Free on mobile

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -290,7 +290,9 @@ export const Navbar = () => {
 									bgColor="blue"
 									className={styles.freeBtn}
 									onClick={() =>
-										handleMobileNavLinkClick(`/signup?email=${email}`)
+										handleMobileNavLinkClick(
+											`/signup?email=${email}`
+										)
 									}
 								/>
 							</div>
